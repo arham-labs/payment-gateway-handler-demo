2ee1ecf0c573176ca8ad0563a504777c421cde87
fawzan <fawzanansari@arhamlabs.com>
2023-01-05 11:21:54 +0530

    [feature] - latest push

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,14 @@
 <?php
 use Arhamlabs\PaymentGateway\Http\Controllers\RazorpayController;
 
-
 Route::get('/test', [RazorpayController::class, 'validation']);
 
 
 Route::get('/callCreateOrder', [RazorpayController::class, 'callCreateOrder']);
+Route::get('/verify-signature', [RazorpayController::class, 'verifySignature']);
+Route::get('/callPayment', [RazorpayController::class, 'callPayment']);
+Route::get('/capture-payment', [RazorpayController::class, 'capturePayment']);
+
+Route::get('update-order-status', [RazorpayController::class, 'updateOrderStatus']);
+Route::get('update-payment-status', [RazorpayController::class, 'updatePaymentStatus']);
+
